b6c07f60f297bedb322a1dfe6c27495b6703d61e
Erik Marty <emarty@broadway.com>
2014-12-06 15:24:52 -0500

    Complete treehouse dictionary challange

--- treehouse/teachers.py
+++ treehouse/teachers.py
@@ -1,11 +1,6 @@
-# The dictionary will be something like:
-# {'Jason Seifer': ['Ruby Foundations', 'Ruby on Rails Forms', 'Technology Foundations'],
-#  'Kenneth Love': ['Python Basics', 'Python Collections']}
-#
-# Often, it's a good idea to hold onto a max_count variable.
-# Update it when you find a teacher with more classes than
-# the current count. Better hold onto the teacher name somewhere
-# too!
+#http://teamtreehouse.com/library/python-collections/dictionaries/teacher-stats
+
+from pprint import pprint
 
 mdict = {
 
@@ -21,6 +16,39 @@ def most_classes(mdict):
   for key, values in mdict.items():
     current_count = len(values)
     if current_count == max_count:
-        return key
+      return key
 
-print(most_classes(mdict))
+def most_classes(mdict):
+  current_count = []
+  max_count = 0
+  for key in mdict:
+    current_count.append(len(mdict[key]))
+  for val in current_count:
+    if val > max_count:
+      max_count = val
+  for key in mdict:
+    if max_count == len(mdict[key]):
+      return key
+
+def num_teachers(mdict):
+  return len(mdict.keys())
+
+def stats(mdict):
+  lst = []
+  for key in mdict:
+    name = key
+    classes = len(mdict[key])
+    lst.append([name, classes])
+  return lst
+
+def courses(mdict):
+  all_courses = []
+  for courses in mdict.values():
+    for course in courses:
+      all_courses.append(course)
+  return all_courses
+
+pprint(most_classes(mdict))
+pprint(num_teachers(mdict))
+pprint(stats(mdict))
+pprint(courses(mdict))

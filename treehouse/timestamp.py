4a99855d2aa370d7389626c971d98799657fb26b
Erik Marty <emarty@broadway.com>
2015-03-31 00:30:06 -0400

    Still trying

--- treehouse/timestamp.py
+++ treehouse/timestamp.py
@@ -2,22 +2,30 @@ import datetime
 
 TS = [1427769570.266099, 1427769191.268007, 1427774014.309288]
 
+
 def timestamp_oldest():
-  #now = datetime.datetime.now()
-  #ts = round(now.timestamp())
-  #frts = [datetime.datetime.fromtimestamp(ts)]
-  #pos = []
-  #for ts in frts:
-  #  pos.append(ts)
-  #  pos.sort()
-  #  for p in pos:
-  #    return p[0]
   pts = []
   for tss in TS:
     frts = datetime.datetime.fromtimestamp(round(tss))
     pts.append(frts)
     pts.sort()
+  #  oldest = str(min(pts))
+  #  timeconv = datetime.datetime.strptime(oldest, '%Y-%m-%d %H:%M:%S')
+  #print(timeconv)
     oldest = min(pts)
-  # convert to strftime here
+  print(oldest)
 
 timestamp_oldest()
+
+
+#def timestamp_oldest(*args):
+#  now = datetime.datetime.now()
+#  ts = round(now.timestamp())
+#  frts = [datetime.datetime.fromtimestamp(ts)]
+#  pos = []
+#  for ts in frts:
+#    pos.append(ts)
+#    pos.sort()
+#    oldest = str(min(pos))
+#    timeconv = datetime.datetime.strptime(oldest, '%Y-%m-%d %H:%M:%S')
+#  return timeconv

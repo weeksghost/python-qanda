212d5aa653e26680997fc2a630c47dc1ad084024
Erik Marty <emarty@broadway.com>
2015-03-31 01:30:32 -0400

    Answered timestamp

--- treehouse/timestamp.py
+++ treehouse/timestamp.py
@@ -6,26 +6,28 @@ TS = [1427769570.266099, 1427769191.268007, 1427774014.309288]
 def timestamp_oldest():
   pts = []
   for tss in TS:
-    frts = datetime.datetime.fromtimestamp(round(tss))
+    frts = datetime.datetime.fromtimestamp(tss)
     pts.append(frts)
     pts.sort()
-  #  oldest = str(min(pts))
-  #  timeconv = datetime.datetime.strptime(oldest, '%Y-%m-%d %H:%M:%S')
-  #print(timeconv)
     oldest = min(pts)
   print(oldest)
 
+  # OR
+
+  print(datetime.datetime.fromtimestamp(sorted(TS)[0]))
+
 timestamp_oldest()
 
+# Treehouse Answer
 
-#def timestamp_oldest(*args):
-#  now = datetime.datetime.now()
-#  ts = round(now.timestamp())
-#  frts = [datetime.datetime.fromtimestamp(ts)]
-#  pos = []
-#  for ts in frts:
-#    pos.append(ts)
-#    pos.sort()
-#    oldest = str(min(pos))
-#    timeconv = datetime.datetime.strptime(oldest, '%Y-%m-%d %H:%M:%S')
-#  return timeconv
+"""
+def timestamp_oldest(*args):
+  pos = []
+  for arg in args:
+    frts = datetime.datetime.fromtimestamp(arg)
+    pos.append(frts)
+    pos.sort()
+    oldest = min(pos)
+  return oldest
+"""
+

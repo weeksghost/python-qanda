f3f4c915f131ae8945fe5527f0ac80da28528c1b
Erik Marty <emarty@broadway.com>
2015-05-29 10:07:43 -0400

    Added mocks for both devices and for loop

--- serial/comtest_2.py
+++ serial/comtest_2.py
@@ -1,10 +1,7 @@
 import serial
 from time import sleep
-from time import gmtime
 from time import strftime
 
-
-COMMAND1 = 'Hello World 1'
 
 device_a = serial.Serial(
             port='/dev/cu.Bluetooth-Incoming-Port',
@@ -31,43 +28,43 @@ def test_connect():
     device_a.open()
 
     # TODO
-    # 1) Create loop of 100 writes + reads
     # 2) Add asserts to verify reads + writes
     # 3) Wrap up in a function
     # 4) Use datetime for micro/miliseconds
 
     if device_a.isOpen():
-        try:
-            port = device_a.port
-            device_a.flushInput()
-            device_a.flushOutput()
-
-            #device_a.write('Chan 1 Send_msg  Hello world 2')
-
-            device_a.write('Hello world 1')
-
-            print('Dev A: [{}] {}'.format(
-                strftime('%H:%M:%S'), 'Chan 0 Send_msg cmd is issued')
-            )
-
-            sleep(.5)
-
-            print('Dev A: [{}] {}'.format(
-                strftime('%H:%M:%S'), 'message sent: Hello world 1')
-            )
-
-            lines = 0
-            while True:
-                response = device_a.readline()
+        for _ in xrange(1):
+            try:
+                port = device_a.port
+                device_a.flushInput()
+                device_a.flushOutput()
+                #device_a.write('Hello world 1') Represents acutal write
+                print('Dev A: [{}] {}'.format(
+                    strftime('%H:%M:%S'), 'Chan 0 Send_msg cmd is issued')
+                )
+                sleep(.5)
+                print('Dev A: [{}] {}'.format(
+                    strftime('%H:%M:%S'), 'message sent: Hello world 1')
+                )
+                #response = device_a.readline() Represents acutal read
                 print('Dev B: [{}] {}'.format(
                     strftime('%H:%M:%S'), 'Chan 0 Received msg: {}'.format(
                         'Hello World 1'))
                 )
-                lines += 1
-                if lines >= 1:
-                    break
-            device_a.close()
-        except serial.SerialException:
-            print('error')
+                print('Dev A: [{}] {}'.format(
+                    strftime('%H:%M:%S'), 'Chan 1 Send_msg cmd is issued')
+                )
+                sleep(.5)
+                print('Dev A: [{}] {}'.format(
+                    strftime('%H:%M:%S'), 'message sent: Hello world 2')
+                )
+                #response = device_a.readline() Represents acutal read
+                print('Dev B: [{}] {}'.format(
+                    strftime('%H:%M:%S'), 'Chan 1 Received msg: {}'.format(
+                        'Hello World 2'))
+                )
+            except serial.SerialException:
+                print('Could not connect to: {}'.format(port))
+        device_a.close()
 
 test_connect()

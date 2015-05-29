5a123dc418a0f30376ab07915775b7a792bd326d
Erik Marty <emarty@broadway.com>
2015-05-29 18:35:53 -0400

    Starting from scratch

--- serial/comtest_2.py
+++ serial/comtest_2.py
@@ -1,100 +1,58 @@
-import serial
 from time import sleep
 from time import strftime
 
 
-device_a = serial.Serial(
-            port='/dev/cu.Bluetooth-Incoming-Port',
-            baudrate=9600,
-            parity=serial.PARITY_ODD,
-            stopbits=serial.STOPBITS_TWO,
-            bytesize=serial.SEVENBITS,
-            timeout=3
-)
+def exec_cmd(device, cmd):
+    for _ in xrange(1, 101):
+        try:
 
-device_b = serial.Serial(
-            port='/dev/cu.Bluetooth-Incoming-Port',
-            baudrate=9600,
-            parity=serial.PARITY_ODD,
-            stopbits=serial.STOPBITS_TWO,
-            bytesize=serial.SEVENBITS,
-            timeout=3
-)
+            # Initialize device(s)
+            device.close()
+            device.open()
+            device.flushInput()
+            device.flushOutput()
+            device.flushInput()
+            device.flushOutput()
 
+            # Get port handle
+            port = device.port
 
-def test_connect():
+            # Assign channels to tuples as mock code for referencing channels
+            channels = 0,1
 
-    device_a.close()
-    device_a.open()
+            # Send command to device
+            send = device.write(cmd)
 
-    # TODO
-    # 2) Add asserts to verify reads + writes
-    # 3) Wrap up in a function
-    # 4) Use datetime for micro/miliseconds
+            # Mock code for distinguishing channel with subsequent optput
+            # based on input
+            if 0 in send:
+                print('Dev A: [{}] Chan 0 Send_msg cmd is issued'.format(
+                        strftime('%H:%M:%S')))
 
-    if device_a.isOpen and device_b.isOpen():
-        for _ in xrange(1):
-            try:
-                port = device_a.port
-                device_a.flushInput()
-                device_a.flushOutput()
-                device_b.flushInput()
-                device_b.flushOutput()
+                # Give recieving device time to process
+                sleep(1)
 
-                # Here I would write actual command to device
-                #send = device_a.write('Hello world 1') Represents acutal write
+                print('Dev A: [{}] message sent: Hello world 1'.format(
+                        strftime('%H:%M:%S')))
 
-                print('Dev A: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 0 Send_msg cmd is issued'))
-                sleep(.5)
-                print('Dev A: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'message sent: Hello world 1'))
+            # If command not through channel 0
+            print('Dev A: [{}] Chan 1 Send_msg cmd is issued'.format(
+                    strftime('%H:%M:%S')))
+            sleep(1)
 
-                # Here I would read the response
-                #recv = device_a.readline() Represents acutal read
-                #if recv != send: # If nothing is sent, print nothing
-                #    print('')
+            print('Dev A: [{}] message sent: Hello world 2'.format(
+                    strftime('%H:%M:%S')))
 
-                print('Dev B: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 0 Received msg: {}'.format(
-                        'Hello World 1\n')))
-                print('Dev A: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 1 Send_msg cmd is issued'))
-                sleep(.5)
-                print('Dev A: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'message sent: Hello world 2'))
-                print('Dev B: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 1 Received msg: {}'.format(
-                        'Hello World 2\n')))
+            # Mock code for recieving device
+            recv = device.readline()
 
-                # Here I would write actual command to device
-                #send = device_b.write('Hello world 1') Represents acutal write
+            if recv != send:
+                print('')
 
-                print('Dev B: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 0 Send_msg cmd is issued'))
-                sleep(.5)
-                print('Dev B: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'message sent: Hello world 1'))
-
-                # Here I would read the response
-                #recv = device_b.readline() Represents acutal read
-                #if recv != send: # If nothing is sent, print nothing
-                #    print('')
-
-                print('Dev A: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 0 Received msg: {}'.format(
-                        'Hello World 1\n')))
-                print('Dev B: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 1 Send_msg cmd is issued'))
-                sleep(.5)
-                print('Dev B: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'message sent: Hello world 2'))
-                print('Dev A: [{}] {}'.format(
-                    strftime('%H:%M:%S'), 'Chan 1 Received msg: {}'.format(
-                        'Hello World 2\n')))
-
-            except serial.SerialException:
-                print('Could not connect to: {}'.format(port))
-        device_a.close()
-
-test_connect()
+            if send in channels[0]:
+                print('Dev B: [{}] Chan 0 Received msg: Hello world 1'.format(
+                    strftime('%H:%M:%S')))
+            print('Dev B: [{}] Chan 1 Received msg: Hello world 2'.format(
+                strftime('%H:%M:%S')))
+        except IOError:
+            print('Could not connect to: {}'.format(port))
